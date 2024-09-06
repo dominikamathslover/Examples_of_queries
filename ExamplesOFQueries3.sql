e795146efef505e4e76eb5d9e21737517c4d4c01
Dominika <dominika.jasinska.lublin@gmail.com>
2024-09-06 13:56:14 +0200

    Update ExamplesOFQueries3.sql

--- ExamplesOFQueries3.sql
+++ ExamplesOFQueries3.sql
@@ -1,5 +1,5 @@
 /*
-Question 31. 
+Question 21. 
 Show all of the patients grouped into weight groups.
 Show the total amount of patients in each weight group.
 Order the list by the weight group decending.
@@ -19,7 +19,7 @@ ORDER BY
 	weight_group DESC;
 
 /*
-Question 32. 
+Question 22. 
 Show patient_id, weight, height, isObese from the patients table.
 Display isObese as a boolean 0 or 1.
 Obese is defined as weight(kg)/(height(m)2) >= 30.
@@ -37,7 +37,7 @@ SELECT
 FROM 	patients
 
 /*
-Question 33. 
+Question 23. 
 Show patient_id, first_name, last_name, and attending doctor's specialty.
 Show only the patients who has a diagnosis as 'Epilepsy' and the doctor's first name is 'Lisa'
 Check patients, admissions, and doctors tables for required information.
@@ -55,7 +55,7 @@ FROM 	patients 	JOIN admissions ON admissions.patient_id = patients.patient_id
 WHERE 	admissions.diagnosis ='Epilepsy'AND doctors.first_name = 'Lisa'
 
 /*
-Question 34. 
+Question 24. 
 All patients who have gone through admissions, can see their medical documents on our site. Those patients are given a temporary password after their first admission. Show the patient_id and temp_password.
 
 The password must be the following, in order:
@@ -72,7 +72,7 @@ SELECT DISTINCT
 FROM 	patients JOIN admissions ON admissions.patient_id = patients.patient_id
 
 /*
-Question 35.
+Question 25.
 Each admission costs $50 for patients without insurance, and $10 for patients with insurance. All patients with an even patient_id have insurance.
 Give each patient a 'Yes' if they have insurance, and a 'No' if they don't have insurance. Add up the admission_total cost for each has_insurance group.
 
@@ -97,7 +97,7 @@ GROUP BY
 		has_insurance;
 
 /*
-Question 36.
+Question 26.
 Show the provinces that has more patients identified as 'M' than 'F'. Must only show full province_name
 
 
@@ -112,7 +112,7 @@ HAVING		COUNT( CASE WHEN gender = 'M' THEN 1 END) > COUNT( CASE WHEN gender = 'F
 
 
 /*
-Question 37.
+Question 27.
 
 We are looking for a specific patient. Pull all columns for the patient who matches the following criteria:
 - First_name contains an 'r' after the first two letters.
@@ -134,7 +134,7 @@ WHERE 	first_name LIKE '__r%' 	AND gender ='F'
 				AND patient_id %2 =1 
 				AND city ='Kingston'
 /*
-Question 38.
+Question 28.
 Show the percent of patients that have 'M' as their gender. Round the answer to the nearest hundreth number and in percent form.
 
 Query:
@@ -145,7 +145,7 @@ SELECT
 FROM 	patients
 
 /*
-Question 39.
+Question 29.
 For each day display the total amount of admissions on that day. Display the amount changed from the previous date.
 
 Query:
@@ -159,7 +159,7 @@ FROM 		admissions
 GROUP BY 	admission_date
 
 /*
-Question 39.
+Question 30.
 Sort the province names in ascending order in such a way that the province 'Ontario' is always on top.
 
 Query:
@@ -172,7 +172,7 @@ ORDER BY 	(CASE WHEN province_name = 'Ontario' THEN 0 ELSE 1 END),
 
 
 /*
-Question 40.
+Question 31.
 
 We need a breakdown for the total amount of admissions each doctor has started each year. Show the doctor_id, doctor_full_name, specialty, year, total_admissions for that year.
 
